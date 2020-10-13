bd471ff58337b0a737c0d3692a3eb4f96d255c52
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-13 17:50:52 +0530

    First Commit

---
+++ src/main/java/com/DSLinkedList/MyNode.java
@@ -0,0 +1,21 @@
+package com.DSLinkedList;
+
+public class MyNode<K> {
+	private K key;
+	private MyNode next;
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+
+	public MyNode(K key) {
+		this.key = key;
+		this.next = null;
+	}
+	
+
+}
