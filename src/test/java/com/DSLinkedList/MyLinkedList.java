384412323dee494b077d910e544889d469a0d2da
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-13 21:03:19 +0530

    Insert inbetween after search

--- src/test/java/com/DSLinkedList/MyLinkedList.java
+++ src/test/java/com/DSLinkedList/MyLinkedList.java
@@ -1,13 +1,22 @@
 package com.DSLinkedList;
 
-public class MyLinkedList {
+
+public class MyLinkedList<K> {
 	public INode head;
 	public INode tail;
-
+	
+	/**
+	 * Constructor
+	 */
 	public MyLinkedList() {
 		this.head = null;
 		this.tail = null;
 	}
+
+	/**
+	 * add at top(UC2)
+	 * @param newNode
+	 */
 	public void add (INode newNode) {
 		if (this.tail == null) {
 			this.tail = newNode;
@@ -21,6 +30,88 @@ public class MyLinkedList {
 
 		}
 	}
+
+	/**
+	 * add from end(UC3)
+	 * @param newNode
+	 */
+	public void append(INode newNode) {
+		if (this.tail == null) {
+			this.tail = newNode;
+		}
+		if(this.head == null) {
+			this.head = newNode;
+		}else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+
+	/**
+	 * insert in between(UC4)
+	 * @param myNode
+	 * @param newNode
+	 */
+	public void insert(INode myNode,INode newNode) {
+		INode tempNode = myNode.getNext() ;
+		myNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
+
+	/**
+	 * Delete element from top(UC5)
+	 * @return
+	 */
+	public INode pop() {
+		INode tempNode = head ;
+		this.head = head.getNext();
+		return tempNode;
+	}  
+
+	/**
+	 * Delete element from end(UC6)
+	 * @return
+	 */
+	public INode popLast() {
+		INode tempNode = head ;
+		while(! tempNode.getNext().equals(tail)) {
+			tempNode = tempNode.getNext();
+		}
+		this.tail = tempNode;
+		tempNode = tempNode.getNext();
+		//tempNode.setNext(null);
+		return tempNode;
+	}
+
+	/**
+	 * Search element(UC7)
+	 * @param key
+	 * @return
+	 */
+	public INode search(K key ) {
+		INode tempNode = head ;
+		while (tempNode != null && tempNode.getNext() != null) {
+			if(tempNode.getKey().equals(key))
+				break;
+			tempNode =tempNode.getNext();
+		}
+		return tempNode;
+	}
+	
+	/**
+	 * Add between specific element(UC8)
+	 * @param key
+	 * @param newNode
+	 */
+	public void addInBetweenSearch(K key, INode newNode) {
+		INode tempINode = search(key);
+		newNode.setNext(tempINode.getNext());
+		tempINode.setNext(newNode);
+	}
+
+	/**
+	 * Print
+	 */
 	public void printMyNode() {
 		StringBuffer print = new StringBuffer(" Node: ");
 		INode tempNode = head;
@@ -33,6 +124,3 @@ public class MyLinkedList {
 		System.out.println(print);
 	}
 }
-
-
-
